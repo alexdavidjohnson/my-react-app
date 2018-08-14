056da332012fe22ef5a659390bc251432ce09c1b
Alex Johnson <heyalxdavid@gmail.com>
2018-08-14 02:11:22 -0700

    First commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Link from 'gatsby-link'
 const IndexPage = () => (
   <div>
     <h1>His people</h1>
-    <p>Welcomwde to your new Gatsby site.</p>
+    <p>Welcomwddde to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
